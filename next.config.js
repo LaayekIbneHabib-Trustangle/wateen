4c7f34c12cdb680edc34bc5e5325cb306e368a68
Afnan Ahmed <i_afnan@icloud.com>
2024-04-16 15:43:24 +0200

    Edit latest config files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
+module.exports = withCSS(withPurgeCss({
+    purgeCssPaths: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  }));
+
 module.exports = withCss(withPurgeCss());
 
 module.exports = {
---
+++ purgecss.config.js
@@ -0,0 +1,7 @@
+// purgecss.config.js
+
+module.exports = {
+    content: ['src/pages/**/*.{js,jsx}', 'src/components/**/*.{js,jsx}'],
+    // Other configuration options can go here
+  };
+  
