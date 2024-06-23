4b2151304d55022b42cf037e6204aa938d174b69
Afnan Ahmed <i_afnan@icloud.com>
2024-06-23 12:33:57 +0300

    Fix bugs

--- src/pages/blog_details/index.jsx
+++ src/pages/blog_details/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Wrapper } from "../../components";
-import RequestADemo from "../../components/request-a-demo";
 import data from "../data/blog-posts.json";
 import "./style.css";
 
